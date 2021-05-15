04a31825c7cc09b66037d49e59f746bef1229c13
sisakov82 <sisakov82@gmail.com>
2021-05-15 10:01:30 +0300

    Добавьте файлы проекта.

---
+++ AuthenticationService/Models/SignUpResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthenticationService.Models
+{
+    public class SignUpResponse
+    {
+        public string Status { get; set; }
+
+        public string Message { get; set; }
+    }
+}
---
+++ AuthenticationService/Startup.cs
@@ -0,0 +1,57 @@
+using AuthenticationBase;
+using AuthenticationBase.Extensions;
+using AuthenticationService.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace AuthenticationService
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            services.AddAppAuthentication(Configuration);
+            services.AddTransient<AppSecurity>();
+            services.AddScoped<IAuthService, AuthService>();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "AuthenticationService", Version = "v1" });
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "AuthenticationService v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
